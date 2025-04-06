c0bff0917b2243befbabcf93336fb8a269031001
Maks Shkafar <m.shkafar@gmail.com>
2025-04-06 17:10:18 +0200

    add serialization and deserialization

--- bot_assistant.py
+++ bot_assistant.py
@@ -1,7 +1,18 @@
 from collections import UserDict
 from datetime import datetime, timedelta
+import pickle
 
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, 'wb') as f:
+        pickle.dump(book, f)
 
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
+   
 
 def input_error(func):
         def inner(*args, **kwargs):
